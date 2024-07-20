303f439dafca645e29c694743d049b2cf8efe675
DoanDinh0324 <110141767+DoanDinh0324@users.noreply.github.com>
2024-07-20 15:12:18 +0700

    push

--- ktra 8.7/js/toge.js
+++ ktra 8.7/js/toge.js
@@ -1,9 +1,34 @@
 $(".menu1").click(function() {
-    $(".b1").toggle();
+    $( ".b1" ).toggle({
+        opacity: 1,
+        height: 'show',
+        marginTop: 'show',
+        marginBottom: 'show',
+        paddingTop: 'show',
+        paddingBottom: 'show'
+      },  
+      1000)
+    
 });
 $(".img2").click(function() {
-    $(".c1").toggle();
+    $( ".c1" ).toggle({
+        opacity: 1,
+        height: 'show',
+        marginTop: 'show',
+        marginBottom: 'show',
+        paddingTop: 'show',
+        paddingBottom: 'show'
+      },  
+      1000)
 });
 $(".im2").click(function() {
-    $(".menu6").toggle();
+    $( ".menu6" ).toggle({
+        opacity: 1,
+        height: 'show',
+        marginTop: 'show',
+        marginBottom: 'show',
+        paddingTop: 'show',
+        paddingBottom: 'show'
+      },  
+      1000)
 });

08457b4d02032e9f44550ab632d15988bf32e3d6
DoanDinh0324 <110141767+DoanDinh0324@users.noreply.github.com>
2024-08-21 18:51:14 +0700

    push

---
+++ shop/js/toggle.js
@@ -0,0 +1,12 @@
+$(".active").click(function() {
+    $( ".order" ).toggle({
+        opacity: 1,
+        height: 'show',
+        marginTop: 'show',
+        marginBottom: 'show',
+        paddingTop: 'show',
+        paddingBottom: 'show'
+      },  
+      1000)
+    
+});

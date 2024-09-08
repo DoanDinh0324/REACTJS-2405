18671a785850bdd163215274a33c8439d278e6f4
DoanDinh0324 <110141767+DoanDinh0324@users.noreply.github.com>
2024-09-08 15:25:59 +0700

    push

--- shop/js/toggle.js
+++ shop/js/toggle.js
@@ -10,3 +10,28 @@ $(".active").click(function() {
       1000)
     
 });
+$("#active").click(function() {
+  $( "#qcvideo" ).toggle({
+      opacity: 1,
+      height: 'show',
+      marginTop: 'show',
+      marginBottom: 'show',
+      paddingTop: 'show',
+      paddingBottom: 'show'
+    },  
+    1000)
+  
+});
+
+$(".qcimg").click(function() {
+  $( ".qcimg1" ).toggle({
+      opacity: 1,
+      height: 'show',
+      marginTop: 'show',
+      marginBottom: 'show',
+      paddingTop: 'show',
+      paddingBottom: 'show'
+    },  
+    1000)
+  
+});

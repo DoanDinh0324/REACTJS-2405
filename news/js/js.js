5282bb0dad623a2ce994cbfdfed1b6671995d1e8
DoanDinh0324 <dinhdoan0324@gmail.com>
2024-10-13 11:03:35 +0700

    news

---
+++ news/js/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".group").click(function() {
+        $("#menu").toggleClass("hidden");
+        $("#menu").toggleClass("opacity-0 translate-y-2");
+    });
+});
+$(document).ready(function() {
+    $(".group1").click(function() {
+        $("#menu1").toggleClass("hidden");
+        $("#menu1").toggleClass("opacity-0 translate-y-2");
+    });
+});
